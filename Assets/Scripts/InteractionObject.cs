6474a53e93b567a654f59eacbda8dcb5421c9f57
LauraS3972523 <114448113+LauraS3972523@users.noreply.github.com>
2022-11-22 17:37:04 +1000

    sound effects added to level
    I've added run jump sound effects to the level

---
+++ Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class InteractionObject : MonoBehaviour
+//{
+  //  public void DoInteraction()
+    //{
+        //picked up and put in inventory
+      //  gameObject.SetActive(false);
+    //}
+
+//}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class PlayerInteract : MonoBehaviour
+//{
+  //  public GameObject currentInterObj = null;
+
+    //public void Update()
+    //{
+      //  if (Input.GetButtonDown("Interact") && currentInterObj)
+        //{
+            //do something with the object
+          //  currentInterObj.SendMessage("DoInteraction");
+        //}
+    //}
+
+//    public void OnTriggerEnter2d(Collider2D other)
+//    {
+//        if (other.CompareTag("InterObject")) ;
+//        Debug.Log(other.name);
+//        currentInterObj = other.GameObject;
+//    }
+ //   public void OnTriggerExit2D(Collider2D other)
+  //  {
+    //    if (other.CompareTag("InterObject"))
+      //  {
+        //    if (other.GameObject == currentInterObj) ;
+          //      currentInterObj = null;
+
+       // }
+    //}
+//}
