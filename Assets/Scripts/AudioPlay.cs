4c55272e08963c49c0f39dbef15062fd54079f83
LauraS3972523 <114448113+LauraS3972523@users.noreply.github.com>
2022-11-24 12:55:18 +1000

    Pop up edit

--- Assets/Scripts/AudioPlay.cs
+++ Assets/Scripts/AudioPlay.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioPlay : MonoBehaviour
+public class Audio_Play : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/TextImporter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextImporter : MonoBehaviour
+{
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    void Start()
+    {
+        if(textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+    }
+
+  }
