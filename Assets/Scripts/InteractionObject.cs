d29748497aab12d49b207634906f1eb5060f62ca
LauraS3972523 <114448113+LauraS3972523@users.noreply.github.com>
2022-11-24 16:58:06 +1000

    Interaction and pop up

--- Assets/Scripts/InteractionObject.cs
+++ Assets/Scripts/InteractionObject.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class InteractionObject : MonoBehaviour
-//{
-  //  public void DoInteraction()
-    //{
-        //picked up and put in inventory
-      //  gameObject.SetActive(false);
-    //}
+public class InteractionObject : MonoBehaviour
+{
+    public GameObject menuPanel;
+    public GameObject messageText;
+    
+    public void DoInteraction()
+    {
+        menuPanel.SetActive(false);
+    }
+ }
 
-//}
--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -2,32 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class PlayerInteract : MonoBehaviour
-//{
-  //  public GameObject currentInterObj = null;
+public class PlayerInteract : MonoBehaviour
+{
+    public GameObject currentInterObj = null;
+    public GameObject menuPanel;
+    public GameObject messageText;
 
-    //public void Update()
-    //{
-      //  if (Input.GetButtonDown("Interact") && currentInterObj)
-        //{
-            //do something with the object
-          //  currentInterObj.SendMessage("DoInteraction");
-        //}
-    //}
+    public void OnTriggerEnter2d(Collider2D other)
+    {
+        if (other.CompareTag("InterObject"))
+        {
+            menuPanel.SetActive(true);
+            currentInterObj = other.gameObject;
+        }
+    }
 
-//    public void OnTriggerEnter2d(Collider2D other)
-//    {
-//        if (other.CompareTag("InterObject")) ;
-//        Debug.Log(other.name);
-//        currentInterObj = other.GameObject;
-//    }
- //   public void OnTriggerExit2D(Collider2D other)
-  //  {
-    //    if (other.CompareTag("InterObject"))
-      //  {
-        //    if (other.GameObject == currentInterObj) ;
-          //      currentInterObj = null;
-
-       // }
-    //}
-//}
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("InterObject"))
+        {
+            if (other.gameObject == currentInterObj) {
+                menuPanel.SetActive(false);
+                currentInterObj = null;
+                    }
+                }
+        }
+}
